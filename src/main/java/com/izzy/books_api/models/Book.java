5c32e094c235ad691412ebe8b625b0bae1b9aeb3
John Doe <johndoe@example.com>
2023-02-23 14:29:49 -0800

    first revision

--- src/main/java/com/izzy/books_api/models/Book.java
+++ src/main/java/com/izzy/books_api/models/Book.java
@@ -1,35 +1,52 @@
 package com.izzy.books_api.models;
-
+import java.util.Date;
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
-
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import org.springframework.format.annotation.DateTimeFormat;
 @Entity
-@Table(name = "books")
+@Table(name="books")
 public class Book {
-
-  @Id
-  @GeneratedValue(strategy = GenerationType.IDENTITY)
-  private Long id;
-
-  private String title;
-  private String author;
-  private Integer pages;
-
-  public Book() {
-  }
-
-  public Book(String title, String author, Integer pages) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-  }
-
-  public String getTitle() {
-    return this.title;
-  }
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Size(min = 5, max = 200)
+    private String title;
+    @NotNull
+    @Size(min = 5, max = 200)
+    private String description;
+    @NotNull
+    @Size(min = 3, max = 40)
+    private String language;
+    @NotNull
+    @Min(100)
+    private Integer numberOfPages;
+    // This will not allow the createdAt column to be updated after creation
+    @Column(updatable=false)
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date createdAt;
+    @DateTimeFormat(pattern="yyyy-MM-dd")
+    private Date updatedAt;
+    
+    public Book() {
+    }
+    public Book(String title, String desc, String lang, int pages) {
+        this.title = title;
+        this.description = desc;
+        this.language = lang;
+        this.numberOfPages = pages;
+    }
+    
+    //  getters and setters
 
   public Long getId() {
     return this.id;
@@ -39,24 +56,63 @@ public class Book {
     this.id = id;
   }
 
+  public String getTitle() {
+    return this.title;
+  }
+
   public void setTitle(String title) {
     this.title = title;
   }
 
-  public String getAuthor() {
-    return this.author;
+  public String getDescription() {
+    return this.description;
   }
 
-  public void setAuthor(String author) {
-    this.author = author;
+  public void setDescription(String description) {
+    this.description = description;
   }
 
-  public Integer getPages() {
-    return this.pages;
+  public String getLanguage() {
+    return this.language;
   }
 
-  public void setPages(Integer pages) {
-    this.pages = pages;
+  public void setLanguage(String language) {
+    this.language = language;
   }
 
+  public Integer getNumberOfPages() {
+    return this.numberOfPages;
+  }
+
+  public void setNumberOfPages(Integer numberOfPages) {
+    this.numberOfPages = numberOfPages;
+  }
+
+  public Date getCreatedAt() {
+    return this.createdAt;
+  }
+
+  public void setCreatedAt(Date createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Date getUpdatedAt() {
+    return this.updatedAt;
+  }
+
+  public void setUpdatedAt(Date updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+
+
+
+    @PrePersist
+    protected void onCreate(){
+        this.createdAt = new Date();
+    }
+    @PreUpdate
+    protected void onUpdate(){
+        this.updatedAt = new Date();
+    }
 }
+
--- src/main/java/com/izzy/books_api/services/BookService.java
+++ src/main/java/com/izzy/books_api/services/BookService.java
@@ -2,24 +2,52 @@
 package com.izzy.books_api.services;
 
 import java.util.List;
+import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.izzy.books_api.models.Book;
 import com.izzy.books_api.repositories.BookRepository;
 
-@Service //<== ANNOTATION for SERVICE REQUIRED for functionality
+@Service
 public class BookService {
-  // LINK to the REPOSITORY vvvvvv
-  @Autowired BookRepository bookRepo;
+  // adding the book repository as a dependency
+  private final BookRepository bookRepository;
 
-  public List<Book> getAll() {
-    //talk to repo
-    return bookRepo.findAll();
+  public BookService(BookRepository bookRepository) {
+    this.bookRepository = bookRepository;
   }
 
-  public Book newBook(Book book) {
-    return bookRepo.save(book);
+  // returns all the books
+  public List<Book> allBooks() {
+    return bookRepository.findAll();
   }
+
+  // creates a book
+  public Book createBook(Book book) {
+    return bookRepository.save(book);
+  }
+
+  // retrieves a book
+  public Book findBook(Long id) {
+    Optional<Book> optionalBook = bookRepository.findById(id);
+    if (optionalBook.isPresent()) {
+      return optionalBook.get();
+    } else {
+      return null;
+    }
+  }
+
+  // updates a book
+  // EXACTLY THE SAME AS CREATE
+  public Book updateBook(Book book) {
+    return bookRepository.save(book);
+  }
+
+  // deletes a book
+  public void deleteBook(Long id) {
+    bookRepository.deleteById(id);
+  }
+
 }
